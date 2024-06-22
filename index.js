71d70b6d880ebe5166b87bc441789015ec96ec40
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-06-22 13:13:00 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = [ "Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {  
+ cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+ cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+cats.shift();
+}
+
+function appendCat() {
+const newappendCat = [...cats, "Broom"];
+return newappendCat
+}
+
+function prependCat() {
+const newCats =  ["Arnold", ...cats];
+return newCats
+}
+
+function removeLastCat() {
+const copyremoveLastCat = cats.slice(0,2);
+return copyremoveLastCat
+}
+
+function removeFirstCat(){
+const copyremoveFirstCat = cats.slice(1);
+return copyremoveFirstCat
+}
